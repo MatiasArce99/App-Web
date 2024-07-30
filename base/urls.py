f8ccc0f18aaf38245bdb51e4b460e684773eebce
Matías <matiasarce214@gmail.com>
2024-07-30 11:37:16 -0300

    Agregando formularios

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('vinilo/', ventana_vinilo, name='vinilo'),
     path('parlante/', ventana_parlante, name='parlante'),
     path('reproductor/', ventana_reproductor, name='reproductor'),
+    path('viniloFormulario/', viniloFormulario, name='viniloFormulario'),
+    path('parlanteFormulario/', parlanteFormulario, name='parlanteFormulario'),
+    path('reproductorFormulario/', reproductorFormulario, name='reproductorFormulario'),
 ]
--- base/views.py
+++ base/views.py
@@ -14,3 +14,16 @@ def ventana_reproductor(request):
     reproductor = Reproductor.objects.all()
     contexto = {'rep': reproductor}
     return render(request, 'ventanas/reproductor.html', contexto)
+
+def viniloFormulario(request):
+    if request.method == 'POST':
+        vinilos = Vinilo(request.POST['artista'], request.POST['album'], request.POST['genero'], request.POST['precio'])
+        vinilos.save()
+        return render(request, 'ventanas/vinilo.html')
+    return render(request, 'ventanas/viniloFormulario.html')
+
+def parlanteFormulario(request):
+    return render(request, 'ventanas/parlanteFormulario.html')
+
+def reproductorFormulario(request):
+    return render(request, 'ventanas/reproductorFormulario.html')
